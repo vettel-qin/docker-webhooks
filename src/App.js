5c5c83ac87a9fc05a9f5ae6e7038209edbec5a02
vettel-qin <vettel_qin@126.com>
2020-09-09 11:51:25 +0800

    'update'

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          update
+          update react
         </p>
         <a
           className="App-link"
