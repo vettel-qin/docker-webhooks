7dbc9956de0f02e6d34f8a080cf0cb01389c3a2e
vettel-qin <vettel_qin@126.com>
2021-02-05 15:43:42 +0800

    'update'

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello react
+          hello react docker
         </p>
       </header>
     </div>
