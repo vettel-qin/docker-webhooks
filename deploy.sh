58bd9579b0c2fea52bdbd9729cbd8c24a6e4abfd
Spencer <Spencer2017@126.com>
2021-03-22 16:46:13 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 git pull
 
 # 停止容器
-docker stop docker-webhooks || true
+/usr/local/bin/docker stop docker-webhooks || true
 
 # 删除容器
-docker rm -f docker-webhooks &> /dev/null || true
+/usr/local/bin/docker rm -f docker-webhooks &> /dev/null || true
 
 # 创建镜像
-docker image build ./ -t docker-webhooks
+/usr/local/bin/docker image build ./ -t docker-webhooks
 
 # 启动容器
-docker run -d --restart=on-failure:5 -p 8084:80 --name docker-webhooks docker-webhooks
+/usr/local/bin/docker run -d --restart=on-failure:5 -p 8084:80 --name docker-webhooks docker-webhooks
