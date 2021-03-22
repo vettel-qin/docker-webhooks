b62b28ce3a78fda854c7bed418e97050d0a0662d
vettel <45277130+vettel-qin@users.noreply.github.com>
2021-03-22 22:11:59 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 git pull
 
 # 停止容器
-/usr/local/bin/docker stop docker-webhooks || true
+docker stop docker-webhooks || true
 
 # 删除容器
-/usr/local/bin/docker rm -f docker-webhooks &> /dev/null || true
+docker rm -f docker-webhooks &> /dev/null || true
 
 # 创建镜像
-/usr/local/bin/docker image build ./ -t docker-webhooks
+docker image build ./ -t docker-webhooks
 
 # 启动容器
-/usr/local/bin/docker run -d --restart=on-failure:5 -p 8084:80 --name docker-webhooks docker-webhooks
+docker run -d --restart=on-failure:5 -p 8084:80 --name docker-webhooks docker-webhooks
